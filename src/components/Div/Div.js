b582f32771b580ed200a844f4e9c70c2c2ff18c4
Ian Price <ianpriceve@gmail.com>
2018-12-09 19:50:54 -0700

    a total overhaul

--- src/components/Div/Div.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './Div.css';
-
-const Div = props => (
-    <div className='div' {...props}>
-    </div>
-);
-
-export default Div;
--- src/scripts/scripts.js
+++
@@ -1,3 +0,0 @@
-$('body').on('click', '#iframe', function(){
-    $("#iframe").attr("src", "http://www.google.com/");
-})
